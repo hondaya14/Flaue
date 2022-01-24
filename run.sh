664f2975fbc22051e6d0b2dd051c761529173598
Yasuhisa Honda <nqvno14@gmail.com>
2022-01-24 13:01:47 +0900

    modify run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ python_path_src="$(pwd)/src"
 python_path_lib="$(pwd)/src/lib"
 python_path_config="$(pwd)/src/config"
 export "PYTHONPATH=$python_path_root:$python_path_src:$python_path_lib:$python_path_config"
-python src/laue.py test_data/intensity_potential.txt
+python src/laue.py test_data/intensity_potential_gt.txt
