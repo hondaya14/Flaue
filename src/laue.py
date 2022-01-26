ca111269221bd2e38187a52d28231c7a99446e78
Yasuhisa Honda <nqvno14@gmail.com>
2022-01-26 17:59:45 +0900

    implement intensities fitting

--- src/laue.py
+++ src/laue.py
@@ -12,28 +12,28 @@ def main(intensity_file_path):
 
     # 強度とその周辺値で強度補正
     for intensity in intensities:
-        pass
+        center = intensity.calculation_center(reciprocal_lattice_vector)
+        fcalc, coord = intensity.pickup_back()
+        print(f'center ... {center}')
+        fcalc.append(intensity.intensity)
+        coord.append(intensity.intensity_coord)
 
-    fitting_param = np.array([10.0, 0.0001])
-    intensity = intensities[0]
+        fcalc, coord = np.array(fcalc), np.array(coord)
+        # print(len(fcalc), len(coord), len(center))
 
-    center = calculation_center(intensity, reciprocal_lattice_vector)
-    fcalc, coord = pickup_back(intensity)
-    print(f'center ... {center}')
-    fcalc.append(intensity.intensity)
-    coord.append(center)
+        fitting_param = np.array([max(fcalc), 0.0001])
+        print(f'fit prm(init) ... {fitting_param}')
+        result = leastsq(objective_function, fitting_param, args=(coord, center, fcalc))
+        print(f'fit prm(fin) ... {result[0]}')
 
-    fcalc, coord = np.array(fcalc), np.array(coord)
-    # print(len(fcalc), len(coord), len(center))
-    result = leastsq(objective_function, fitting_param, args=(coord, center, fcalc))
-    print(f'result ... {result}')
+        after_calc = gaussian(result[0], coord, center)
 
-    after_calc = gaussian(result[0], coord, center)
+        print(f'Fcalc\t\tFcalc(fitting)')
+        for i in range(7):
+            print(f'{str(fcalc[i]).rjust(8)}\t{round(after_calc[i], 6)}')
+        print(f'--------------------------------------------------------')
 
-    for i in range(7):
-        print(f'{fcalc[i]} {after_calc[i]}')
-
-    draw_gaussian_2dim(result, coord, fcalc, center)
+    # draw_gaussian_2dim(result, coord, fcalc, center)
 
 
 if __name__ == "__main__":
